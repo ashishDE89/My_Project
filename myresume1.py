b07caf18fc05fa766a6ed8ade7030d40a9c922c3
ashishDE89 <123041092+ashishDE89@users.noreply.github.com>
2023-02-25 16:15:44 -0800

    Update myresume1.py

--- myresume1.py
+++ myresume1.py
@@ -146,27 +146,3 @@ with about_me:
              "and love to cook, My curiosity about the world makes me adventurous so love hiking and attend social gatherings. "
              "A new found passion towards Pickle Ball pushes me hard to dive deep into learning the tricks of the game"
               "To top it all, I am avid yoga practioner")
-
-
-def load_lottieurl(url: str):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
-
-url = "https://assets1.lottiefiles.com/packages/lf20_cxCtsKKkFK.json"
-
-lottie_hello = load_lottieurl(url)
-
-st_lottie(
-    lottie_hello,
-    speed=1,
-    reverse=False,
-    loop=True,
-    quality="low",  # medium ; high
-    height=200,
-    width=200,
-    key=None,
-)
-
-load_lottieurl(url)
